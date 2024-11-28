c1cb0c41ddf86765cd1612b13a66de6d69539af3
anggiperwitasari <76466849+anggiperwitasari@users.noreply.github.com>
2024-11-28 13:26:07 +0700

    Create LibMath.cpp

---
+++ LibMath.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath> // Library math
+using namespace std;
+
+int main() {
+    double angka = 25.0;
+    cout << "Akar kuadrat dari " << angka << " adalah " << sqrt(angka) << endl;
+    cout << "2 pangkat 3 adalah " << pow(2, 3) << endl;
+    return 0;
+}
