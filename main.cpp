184493fec364cd1623db89f91dd1bd6e7d0900b4
anggiperwitasari <76466849+anggiperwitasari@users.noreply.github.com>
2024-11-28 13:40:06 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "math_tools.h" // Include header file
+using namespace std;
+
+int main() {
+    cout << "5 + 3 = " << tambah(5, 3) << endl;
+    cout << "5 * 3 = " << kali(5, 3) << endl;
+    return 0;
+}
