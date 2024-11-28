c4b215ddd76d63bcf38817e23ef802753b620b54
anggiperwitasari <76466849+anggiperwitasari@users.noreply.github.com>
2024-11-28 13:19:30 +0700

    Create Array.c

---
+++ Array.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {10, 20, 30, 40, 50};
+    printf("Elemen-elemen array adalah:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
